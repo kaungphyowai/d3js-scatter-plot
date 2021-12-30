75671ac59d3868e313268c1482f6751d8ff7ec91
Kaung Phyo Wai <kaungphyowai.mm@gmail.com>
2021-12-30 17:00:54 +0630

    x axis and y axis complete

--- index.js
+++ index.js
@@ -1,35 +1,47 @@
 let width = 1000;
 let height = 700;
-let horizontalPadding = 40;
+let horizontalPadding = 100;
 let verticalPadding = 30;
+let xStratPoint = horizontalPadding;
+let xEndPoint = width - horizontalPadding;
+let yStartPoint = verticalPadding;
+let yEndPoint = height - verticalPadding;
 //find the min value of year
 fetch("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json")
     .then(data => data.json()).then(res => {
+        //Format Data
+        let yearsRange = res.map(data => {
+            return new Date(data['Year'].toString());
+        })
+        let records = res.map(data => {
+            return new Date(data['Seconds'] * 1000)
+        })
+        //Draw An Svg
         let svg = d3.select('svg').attr('width', width).attr('height', height);
-        //draw the Xaxis
-        let yearMin = d3.min(res, d => d['Year'])
-        let yearMax = d3.max(res, d => d['Year'])
-        console.log(yearMax)
-        let xScale = d3.scaleLinear().domain([yearMin - 1, yearMax]).range([0 + horizontalPadding, width - horizontalPadding])
+        //Draw the Xaxis
+        let yearMin = d3.min(yearsRange)
+        let yearMax = d3.max(yearsRange)
+        let xScale = d3.scaleLinear().domain([yearMin.getFullYear() - 1, yearMax.getFullYear() + 1]).range([xStratPoint, xEndPoint ])
         let Xaxis = d3.axisBottom(xScale)
         svg.append('g')
-            .attr("transform", "translate(" + 0 + ", " + (height - verticalPadding) + ")")
+            .attr("transform", "translate(" + 0 + ", " + yEndPoint + ")")
             .attr('id', 'x-axis')
             .call(Xaxis)
 
 
-        //draw the Yaxis
-        let timeMin = d3.min(res, d => d['Time']);
-        let timeMax = d3.max(res, d => d['Time']);
+        // //draw the Yaxis
+        let timeMin = d3.min(records);
+        let timeMax = d3.max(records);
+        timeMax.setSeconds(timeMax.getSeconds() + 1)
         var Yscale = d3.scaleLinear()
             .domain([timeMin, timeMax])
-            .range([0 + verticalPadding, height - verticalPadding]);
-
-        var y_axis = d3.axisLeft(Yscale);
+            .range([yStartPoint, yEndPoint]);
+            
+        var y_axis = d3.axisLeft(Yscale).tickFormat(d3.utcFormat("%M:%S"));
 
 
         svg.append("g")
             .attr('id', 'y-axis')
-            .attr('transform', 'translate(' + horizontalPadding + ", " + 0 + ")")
+            .attr('transform', 'translate(' + xStratPoint + ", " + 0 + ")")
             .call(y_axis);
     })
